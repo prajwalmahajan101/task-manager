8ce6f032cae971e8a47488490830bb434ee6f32d
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-12-03 11:07:54 +0530

    SignIn Validation Added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService, SignInReturnType } from './auth.service';
+import { SignInDto } from './dto/signInDto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>): Promise<SignInReturnType> {
-    //TODO: Add Validation
+  signIn(@Body() signInDto: SignInDto): Promise<SignInReturnType> {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 }
